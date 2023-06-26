a6131cdde77e6c7d543333482f72c52846f72a64
Acacio Souza da Silva <zadfcer666@gmail.com>
2023-06-26 10:39:13 -0300

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', function() {
-    navigator.serviceWorker.register('/service-worker.js')
+    navigator.serviceWorker.register('./service-worker.js')
       .then(function(registration) {
         console.log('Service Worker registrado com sucesso:', registration);
       })
